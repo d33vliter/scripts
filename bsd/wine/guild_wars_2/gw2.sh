49582dcfdfc2dcfd79cd3c079b218ee687fd411d
d33vliter <iandupuy1998@gmail.com>
2025-09-29 17:14:08 -0500

    Update gw2.sh

--- bsd/wine/guild_wars_2/gw2.sh
+++ bsd/wine/guild_wars_2/gw2.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 ruta='/media/external/bsd/Guild Wars 2' #cambiala a la ruta donde hayas instalado GW2
 WINEPREFIX="$HOME/.wine"
-WINEESYNC=0
-WINEFSYNC=1
+WINEESYNC=1
 WINEDEBUG=-all
 DXVK_HUD="devinfo,fps,gpuload,compiler"
 DXVK_STATE_CACHE=1
-export WINEPREFIX DXVK_HUD DXVK_STATE_CACHE WINEESYNC WINEFSYNC WINEDEBUG
+export WINEPREFIX DXVK_HUD DXVK_STATE_CACHE WINEESYNC WINEDEBUG
 
 cd "$ruta" || { echo "No se pudo acceder a $ruta"; exit 1; } 
 
